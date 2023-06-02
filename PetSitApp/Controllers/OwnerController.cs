b63e7a1b872beff2456be07880a5cdf716af688e
Zeshan Munir <zmunir93@gmail.com>
2023-06-02 03:56:36 -0400

    Reached database in Owner Controller.

---
+++ PetSitApp/Controllers/OwnerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetSitApp.Data;
+
+namespace PetSitApp.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public OwnerController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objOwnerList = _db.Owners.ToList();
+            return View();
+        }
+    }
+}
