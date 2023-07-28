4e468b7c87d225b72e563edc6f546da7ce174239
Zeshan Munir <zmunir93@gmail.com>
2023-07-28 05:33:23 -0400

    User now being created to database. Passwords are hashed. Renamed User Index to User Register in controller and pages. Added a temp User Index.

--- PetSitApp/Controllers/LoginController.cs
+++ PetSitApp/Controllers/LoginController.cs
@@ -6,28 +6,30 @@ namespace PetSitApp.Controllers
 {
     public class LoginController : Controller
     {
-        private readonly ApplicationDBContext _db;
-        public LoginController(ApplicationDBContext db)
-        {
-            _db = db;
-        }
+        //private readonly ApplicationDBContext _db;
+        //public LoginController(ApplicationDBContext db)
+        //{
+        //    _db = db;
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(string username, string password)
+        //{
+        //    var user = _db.Users.FirstOrDefault(user => user.Username.ToLower().Equals(username));
+
+        //    //if (result.Succeeded)
+        //    //{
+        //    //    return RedirectToAction("index", "home");
+        //    //}
+
+        //    return View();
+        //}
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Index(string username, string password)
-        {
-            var user = _db.Users.FirstOrDefault(user => user.Username.ToLower().Equals(username));
-
-            //if (result.Succeeded)
-            //{
-            //    return RedirectToAction("index", "home");
-            //}
-
-            return View();
-        }
+        
     }
 
 
--- PetSitApp/Controllers/UserController.cs
+++ PetSitApp/Controllers/UserController.cs
@@ -1,6 +1,63 @@
-﻿namespace PetSitApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using PetSitApp.Data;
+using PetSitApp.Models;
+
+namespace PetSitApp.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public UserController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                // checks to see if user exist in database
+                var existingUser = _db.Users.FirstOrDefault(u => u.Username.ToLower().Equals(model.Username.ToLower()));
+
+                // if they do exist, error is thrown
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("", "Username already exist");
+                    return View(model);
+                }
+
+                // model.Password is encrypted
+                var passwordHash = BCrypt.Net.BCrypt.HashPassword(model.Password);
+
+                // newUserObj created to pass info to be added to database
+                var newUserObj = new User
+                {
+                    Username = model.Username,
+                    Password = passwordHash
+                };
+
+                // info is added and then saved. Redirected to index page
+                _db.Users.Add(newUserObj);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View(model);
+            }
+        }
     }
 }
