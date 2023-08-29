163279304721b06434aac0c39061c9bfcd3eac7d
Zeshan Munir <zmunir93@gmail.com>
2023-08-29 02:40:55 -0400

    Successfully able to edit and delete pet from owner.

--- PetSitApp/Controllers/PetController.cs
+++ PetSitApp/Controllers/PetController.cs
@@ -15,14 +15,37 @@ namespace PetSitApp.Controllers
             _db = db;
         }
 
-        
+        // GET
         public IActionResult CreatePet()
         {
             
             return View();
         }
-       
+        // GET
+        public async Task<IActionResult> EditPet(int? id) 
+        {
+            var petFromDb = await _db.Pets.FindAsync(id);
+
+            if (petFromDb == null)
+            {
+                return NotFound();
+            }
+          
+            return View(petFromDb);
+        }
+        // GET
+        public async Task<IActionResult> DeletePet(int? id)
+        {
+            var petFromDb = await _db.Pets.FindAsync(id);
+            if (petFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(petFromDb);
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> CreatePet(Pet model)
         {
             ModelState.Remove("Owner");
@@ -43,6 +66,43 @@ namespace PetSitApp.Controllers
                 return RedirectToAction("Dashboard", "Owner");
             }
             return View(model);      
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPet(int id, Pet model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            ModelState.Remove("Owner");
+            if (ModelState.IsValid)
+            {
+                _db.Update(model);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Successfully updated pet";
+                return RedirectToAction("Dashboard", "Owner");
+            }
+            return View(model);  
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePet(int id, Pet model)
+        {
+            var pet = await _db.Pets.FindAsync(id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            _db.Remove(pet);
+            await _db.SaveChangesAsync();
+            TempData["success"] = "Successfully deleted Pet";
+            return RedirectToAction("Dashboard", "Owner");
+
         }
     }
 }
