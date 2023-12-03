cd450aeb69b7398860484e888c46e6e28febdef0
Zeshan Munir <zmunir93@gmail.com>
2023-12-02 23:05:05 -0500

    Stripe payment pulls data from search and sitter. checkout is successful.

---
+++ PetSitApp/Controllers/CheckoutController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetSitApp.ViewModels;
+using Stripe.Checkout;
+
+namespace PetSitApp.Controllers
+{
+    public class CheckoutController : Controller
+    {
+
+        public IActionResult Confirmation()
+        {
+            var service = new SessionService();
+            Session session = service.Get(TempData["Session"].ToString());
+            if (session.PaymentStatus == "paid")
+            {
+                return View("Success");
+            }
+            return View("Failure");
+        }
+
+        /////////////////////////// GET
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Failure()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout(int quantity, int rate)
+        {
+            var domain = "https://localhost:7112/";
+
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = domain + $"Checkout/Confirmation",
+                CancelUrl = domain + $"Checkout/Unsuccessful",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                CustomerEmail = "email@email.com"
+            };
+
+            var sessionListItem = new SessionLineItemOptions
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    UnitAmount = rate,
+                    Currency = "usd",
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = "Dog Sitting"
+                    }
+                },
+                Quantity = quantity,
+
+            };
+
+            options.LineItems.Add(sessionListItem);
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            TempData["Session"] = session.Id;
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+
+
+            
+        }
+    }
+}
