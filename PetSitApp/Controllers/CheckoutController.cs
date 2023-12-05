bac2c24ca65bee2fe2b52d5d8c26b429da0dc614
Zeshan Munir <zmunir93@gmail.com>
2023-12-05 04:19:55 -0500

    Fixed Rate amount on Checkout. Fixed ServiceType/jobType on Checkout. Checkout now makes sure Owner is logged in to checkout now.

--- PetSitApp/Controllers/CheckoutController.cs
+++ PetSitApp/Controllers/CheckoutController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PetSitApp.ViewModels;
 using Stripe.Checkout;
+using System.Security.Claims;
 
 namespace PetSitApp.Controllers
 {
@@ -24,13 +25,22 @@ namespace PetSitApp.Controllers
             return View();
         }
 
-        public IActionResult Failure()
+        public IActionResult Unsuccessful()
         {
             return View();
         }
 
-        public IActionResult Checkout(int quantity, int rate)
+        public IActionResult Checkout(int quantity, int rate, string jobType)
         {
+
+            var loggedOwner = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (loggedOwner == null)
+            {
+                TempData["error"] = "Please login as an Owner";
+                return RedirectToAction("Login", "User");
+                
+            }
+
             var domain = "https://localhost:7112/";
 
             var options = new SessionCreateOptions
@@ -50,7 +60,7 @@ namespace PetSitApp.Controllers
                     Currency = "usd",
                     ProductData = new SessionLineItemPriceDataProductDataOptions
                     {
-                        Name = "Dog Sitting"
+                        Name = jobType
                     }
                 },
                 Quantity = quantity,
