9f5642553a5d06a995669d2b11e90c03da4f878e
Zeshan Munir <zmunir93@gmail.com>
2024-01-07 03:13:44 -0500

    added js to login page. now properly switches to the sitter login and back to the owner login.

--- PetSitApp/wwwroot/js/site.js
+++ PetSitApp/wwwroot/js/site.js
@@ -32,55 +32,5 @@ document.getElementById('home-check').addEventListener('change', function () {
     }
 });
 
-//// Initialize and add the map
-//let map;
-
-//async function initMap() {
-//    const position = { lat: 40.758896, lng: -73.985130 };
-//    //Request needed libraries
-//    const { Map } = await google.maps.importLibrary("maps");
-//    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-//    // The map, centered
-//    map = new Map(document.getElementById("map"), {
-//        zoom: 4,
-//        center: position,
-//        mapId: "DEMO_MAP_ID",
-//    });
-
-//    // Map's marker position
-//    const marker = new AdvancedMarkerElement({
-//        map: map,
-//        position: position,
-//        title: "NY"
-//    });
-//}
-
-//initMap();
-
-//let map;
-
-//async function initMap() {
-//    const { Map } = await google.maps.importLibrary("maps");
-
-//    map = new Map(document.getElementById("map"), {
-//        center: { lat: -34.397, lng: 150.644 },
-//        zoom: 8,
-//    });
-//}
-//await initMap();
-
-//let map;
-
-//function initMap() {
-//    return google.maps.importLibrary("maps").then(({ Map }) => {
-//        map = new Map(document.getElementById("map"), {
-//            center: { lat: -34.397, lng: 150.644 },
-//            zoom: 8,
-//        });
-//    });
-//}
-
-//initMap(); // No need for await if not in an async function
 
 
